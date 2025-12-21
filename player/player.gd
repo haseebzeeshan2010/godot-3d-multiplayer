e29436de2969b6a5929a691849fb88bdf8255cf2
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-12-21 15:11:03 +0000

    Make player jump and move

--- player/player.gd
+++ player/player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
+#@export exposes values to godot's inspector
 
+@export var player_sprite: AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var movement_speed: float = 300.0
+@export var gravity = 30.0
+@export var jump_strength = 600.0
 
+@onready var initial_sprite_scale = player_sprite.scale
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#Move the Player
+	var horizontal_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = horizontal_input * movement_speed
+	velocity.y += gravity
+	
+	#Make the Player Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_strength
 
 	move_and_slide()
+	
+	#Flip the Player
+	if not is_zero_approx(horizontal_input):
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		elif horizontal_input > 0:
+			player_sprite.scale = initial_sprite_scale
+	
