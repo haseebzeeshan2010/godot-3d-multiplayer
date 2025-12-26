16e7e6d8eedced4fcb78fb9af91aa7b0bb3db3b5
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-12-26 14:17:36 +0000

    Add a networked pressure plate that is server authoritative

---
+++ interactables/pressure_plate/pressure_plate.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var plate_up: Sprite2D
+@export var plate_down: Sprite2D
+@export var is_down: bool = false
+
+
+
+var bodies_on_plate = 0
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if not multiplayer.is_server(): # Only the server should manage the plate state
+		return
+	bodies_on_plate += 1
+	update_plate_state()
+
+
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	if not multiplayer.is_server(): # Only the server should manage the plate state
+		return
+	bodies_on_plate -= 1
+	update_plate_state()
+
+
+func update_plate_state() -> void:
+	is_down = bodies_on_plate >= 1
+	set_plate_properties()
+
+func set_plate_properties() -> void:
+	plate_down.visible = is_down
+	plate_up.visible = not is_down
+
+func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+	set_plate_properties()
