0e5e2cec98f7443ac2104cc84d7fceb3756a5b67
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-12-26 15:00:28 +0000

    Add door interactable that opens/closes on pressure plate state change

--- interactables/pressure_plate/pressure_plate.gd
+++ interactables/pressure_plate/pressure_plate.gd
@@ -1,34 +1,35 @@
 extends Node2D
 
+signal toggle(state) # Set up signal
+
+@export var is_down = false
 @export var plate_up: Sprite2D
 @export var plate_down: Sprite2D
-@export var is_down: bool = false
-
-
 
 var bodies_on_plate = 0
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-	if not multiplayer.is_server(): # Only the server should manage the plate state
+func _on_area_2d_body_entered(_body):
+	if not multiplayer.is_server(): # Only the server should manage state
 		return
 	bodies_on_plate += 1
 	update_plate_state()
 
-
-func _on_area_2d_body_exited(_body: Node2D) -> void:
-	if not multiplayer.is_server(): # Only the server should manage the plate state
+func _on_area_2d_body_exited(_body):
+	if multiplayer.multiplayer_peer == null:
+		return
+	if not multiplayer.is_server(): # Only the server should manage state
 		return
 	bodies_on_plate -= 1
 	update_plate_state()
 
-
-func update_plate_state() -> void:
+func update_plate_state():
 	is_down = bodies_on_plate >= 1
+	toggle.emit(is_down) # Emit signal with state
 	set_plate_properties()
 
-func set_plate_properties() -> void:
+func set_plate_properties():
 	plate_down.visible = is_down
-	plate_up.visible = not is_down
+	plate_up.visible = !is_down
 
-func _on_multiplayer_synchronizer_delta_synchronized() -> void:
+func _on_multiplayer_synchronizer_synchronized():
 	set_plate_properties()
